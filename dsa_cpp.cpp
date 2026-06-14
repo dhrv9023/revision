002e4ffbd9835f418283b9a0fef2fa8b5758fc35
dhrv9023 <dhrv9023@gmail.com>
2026-06-14 13:03:56 +0530

    revision Daily Update 2026-06-14 13:03:56

--- dsa_cpp.cpp
+++ dsa_cpp.cpp
@@ -15,19 +15,42 @@ using namespace std;
 
 // SECOND LARGEST IN AN ARRAY
 
+// int main(){
+//     vector<int> ar={1,2,6,3,7,4,2};
+//     int largest = 0;
+//     int slargest = INT_MIN;
+//     for(int i =0;i<ar.size();i++){
+//         if(ar[i]>largest){
+//             slargest=largest;
+//             largest=ar[i];
+            
+//         }
+//         else if (ar[i]<largest && slargest < ar[i]){
+//             slargest=ar[i];
+//         }
+//     }
+//     cout<<slargest<<endl;
+// }
+
+
+// 2 SUM (RETURN NUMBERS)
+
 int main(){
     vector<int> ar={1,2,6,3,7,4,2};
-    int largest = 0;
-    int slargest = INT_MIN;
-    for(int i =0;i<ar.size();i++){
-        if(ar[i]>largest){
-            slargest=largest;
-            largest=ar[i];
-            
+    sort(ar.begin(),ar.end());
+    int target= 5;
+    int k=0;
+    int j = ar.size()-1;
+    while(k<j){
+        if (ar[k]+ar[j]==target){
+            cout<<ar[k]<<","<< ar[j]<<endl;
+            break;
         }
-        else if (ar[i]<largest && slargest < ar[i]){
-            slargest=ar[i];
+        else if(ar[k]+ar[j]>target){
+            j--;
+        }
+        else{
+            k++;
         }
     }
-    cout<<slargest<<endl;
 }
