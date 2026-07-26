d3769af8912ce598c964db116219d0f944a22971
dhrv9023 <dhrv9023@gmail.com>
2026-07-26 12:55:42 +0530

    revision Daily Update 2026-07-26 12:55:41

--- python_dsa.py
+++ python_dsa.py
@@ -8,48 +8,47 @@
 
 # 2 SUM
 
-def two_sum(nums,target):
-    hshmpp={}
-    for index,value in enumerate(nums):
-        diff= target-value
-        if diff in hshmpp:
-            return {hshmpp[diff],index}
-        hshmpp[value]=index
+# def two_sum(nums,target):
+#     hshmpp={}
+#     for index,value in enumerate(nums):
+#         diff= target-value
+#         if diff in hshmpp:
+#             return {hshmpp[diff],index}
+#         hshmpp[value]=index
     
-nums=[2,7,11,15]
-target = 9
-print(f"the index of the {target} is ",end=' ')
-print(two_sum(nums,target))    
-
-#MAXIMUM SUBARRAY
-
-# def sbarr(nums):
-#     mxsub=nums[0]
-#     cursum=0
-#     for i in nums:
-#         if cursum < 0:
-#             cursum=0
-#         cursum+=i
-#         mxsub=max(mxsub,cursum)
-#     return mxsub    
+# nums=[2,7,11,15]
+# target = 9
+# print(f"the index of the {target} is ",end=' ')
+# print(two_sum(nums,target))    
 
 
 
-# nums=[-2,1,-3,4,-1,2,1,-5,4]
-# print(sbarr(nums))
-
-
-#CONTINS DUPLICATE
+#contains duplicate
 
 # def dup(nums):
-#     hsset=set()
+#     hshm=set()
 #     for i in nums:
-#         if i in hsset:
+#         if i in hshm:
 #             return True
-#         hsset.add(i)
+#         hshm.add(i)
 #     return False    
 
 
-# nums = [1,2,3,6]    
-# print(dup(nums))
+# nums=[1,2,3,5,4] 
+# print(dup(nums))   
 
+
+#maxsubarray sum
+
+# def sm(nums):
+#     currsum=0
+#     maxsm=nums[0]
+#     for i in nums:
+#         if currsum<0:
+#             currsum=0
+#         currsum=currsum+i
+#         maxsm=max(currsum,maxsm)
+#     return maxsm
+        
+# nums=[-2,1,-3,4,-1,2,1,-5,4]
+# print(sm(nums))   
