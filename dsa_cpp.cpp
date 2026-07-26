7e6c42cbd87501204f2691a60122bd725c4ee964
dhrv9023 <dhrv9023@gmail.com>
2026-07-26 13:47:23 +0530

    revision Daily Update 2026-07-26 13:47:23

--- dsa_cpp.cpp
+++ dsa_cpp.cpp
@@ -1,22 +1,23 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int largest_element(vector<int>arr){
-    int largest=0;
-    int slargest=-1;
-    for(int i=0;i<arr.size();i++){
-        if (arr[i] > largest){
-            slargest=largest;
-            largest=arr[i];
-        }
-        if(arr[i]>slargest && arr[i]<largest){
-            slargest=arr[i];
+// int largest_element(vector<int>arr){
+//     int largest=0;
+//     int slargest=-1;
+//     for(int i=0;i<arr.size();i++){
+//         if (arr[i] > largest){
+//             slargest=largest;
+//             largest=arr[i];
+//         }
+//         if(arr[i]>slargest && arr[i]<largest){
+//             slargest=arr[i];
             
-        }
-    }
-    return slargest;
-}
-int main(){
-    vector<int> arr={2,5,4,1,6,1};
-    cout<<largest_element(arr);
-}
+//         }
+//     }
+//     return slargest;
+// }
+// int main(){
+//     vector<int> arr={2,5,4,1,6,1};
+//     cout<<largest_element(arr);
+// }
+
