ddaaceb380d9ab398b536998a6e16c782c8305b3
dhrv9023 <dhrv9023@gmail.com>
2026-07-26 13:44:13 +0530

    revision Daily Update 2026-07-26 13:44:13

--- dsa_cpp.cpp
+++ dsa_cpp.cpp
@@ -1,56 +1,21 @@
-// LARGEST NUMBER IN AN ARRAY
-
 #include<bits/stdc++.h>
 using namespace std;
-// int main(){
-//     vector<int> ar={1,2,6,3,7,4,2};
-//     int largest = 0;
-//     for(int i=0;i<ar.size();i++){
-//         if (ar[i]>largest){
-//             largest =ar[i];
-//         }
-//     }
-//     cout<<largest<<endl;
-// }
 
-// SECOND LARGEST IN AN ARRAY
-
-// int main(){
-//     vector<int> ar={1,2,6,3,7,4,2};
-//     int largest = 0;
-//     int slargest = INT_MIN;
-//     for(int i =0;i<ar.size();i++){
-//         if(ar[i]>largest){
-//             slargest=largest;
-//             largest=ar[i];
-            
-//         }
-//         else if (ar[i]<largest && slargest < ar[i]){
-//             slargest=ar[i];
-//         }
-//     }
-//     cout<<slargest<<endl;
-// }
-
-
-// 2 SUM (RETURN NUMBERS)
-
-// int main(){
-//     vector<int> ar={1,2,6,3,7,4,2};
-//     sort(ar.begin(),ar.end());
-//     int target= 5;
-//     int k=0;
-//     int j = ar.size()-1;
-//     while(k<j){
-//         if (ar[k]+ar[j]==target){
-//             cout<<ar[k]<<","<< ar[j]<<endl;
-//             break;
-//         }
-//         else if(ar[k]+ar[j]>target){
-//             j--;
-//         }
-//         else{
-//             k++;
-//         }
-//     }
-// }
+int largest_element(vector<int>arr){
+    int largest=0;
+    int slargest=-1;
+    for(int i=0;i<arr.size();i++){
+        if (arr[i] > largest){
+            largest=arr[i];
+        }
+        if(arr[i]>largest && slargest<largest){
+            largest=arr[i];
+            slargest=largest;
+        }
+    }
+    return slargest;
+}
+int main(){
+    vector<int> arr={2,5,4,1,6,1};
+    cout<<largest_element(arr);
+}
